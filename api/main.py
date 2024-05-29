61d99073bffe36fdae156fc24cc407f4dbc459dd
MULIARR <10328055@stud.op.edu.ua>
2024-05-29 21:59:08 +0300

    Add CORS support

--- api/main.py
+++ api/main.py
@@ -2,8 +2,19 @@ import logging
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Настройка CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 logger = logging.getLogger(__name__)
 
 
