bbd76c02c2d7057b6ef9a5f32e2fe55a98db0697
MULIARR <10328055@stud.op.edu.ua>
2024-05-30 12:53:00 +0300

    Initial commit with minimal example

--- api/main.py
+++ api/main.py
@@ -1,48 +1,8 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 
 app = FastAPI()
 
-# Настройка CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-
-class User(BaseModel):
-    user_id: int
-
-
-@app.post("/save_user_id")
-async def save_user_id(user: User):
-    try:
-        return {"status": "success", "user_id": user.user_id}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@app.get("/hello")
+@app.get("/api/hello")
 async def read_hello():
-    try:
-        return {"message": "Hello, World!"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-
-@app.get("/")
-async def read_root():
-    return {"message": "Hello from the root endpoint"}
-
-
-if __name__ == "__main__":
-    import hypercorn.asyncio
-    from hypercorn.config import Config
-
-    config = Config()
-    config.bind = ["0.0.0.0:8000"]
-    hypercorn.asyncio.run_single(app, config)
+    return {"message": "Hello, World!"}
