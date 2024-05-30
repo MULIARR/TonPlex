ac3d6eba69cc874ea1572e2106561c1918946dc0
MULIARR <10328055@stud.op.edu.ua>
2024-05-30 11:24:43 +0300

    Fixed local server issue and updated configuration for Vercel

--- api/main.py
+++ api/main.py
@@ -6,20 +6,25 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Настройка CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# # Настройка CORS
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 logger = logging.getLogger(__name__)
 
 
 class User(BaseModel):
     user_id: int
+
+
+@app.get("/hello")
+async def read_root():
+    return {"Hello": "World"}
 
 
 @app.post("/save_user_id")
@@ -30,12 +35,3 @@ async def save_user_id(user: User):
         "status": "success",
         "user_id": user.user_id
     }
-
-
-if __name__ == "__main__":
-    import hypercorn.asyncio
-    from hypercorn.config import Config
-
-    config = Config()
-    config.bind = ["0.0.0.0:8000"]
-    hypercorn.asyncio.run_single(app, config)
