a318c1c7c759201e8271f16a6b18a81ec3d4b618
MULIARR <10328055@stud.op.edu.ua>
2024-06-02 12:51:27 +0300

    Check WebTg integration

--- backend/app/routers/wallet_setup.py
+++ backend/app/routers/wallet_setup.py
@@ -5,17 +5,35 @@ from backend.config import config
 
 templates = Jinja2Templates(directory=config.app.TEMPLATES_DIR)
 
-
 wallet_setup_router = APIRouter(
     prefix="/wallet_setup"
 )
 
 
-@wallet_setup_router.get("")
+@wallet_setup_router.get("/create")
 async def get_wallet_setup(request: Request):
 
+    # TODO: create wallet with tontools
+    wallet_mnemonics = [
+        "apple", "bread", "clock", "dance", "eagle", "flame",
+        "grape", "house", "index", "jumps", "kites", "lemon",
+        "mango", "night", "olive", "plant", "queen", "river",
+        "sunny", "trees", "under", "vivid", "wings", "zebra"
+    ]
+
     return templates.TemplateResponse(
-        "welcome.html",
+        "create_wallet.html",
+        {
+            "wallet_mnemonic_phrase": wallet_mnemonics,
+            "request": request
+        }
+    )
+
+
+@wallet_setup_router.get("/import")
+async def get_wallet_setup(request: Request):
+    return templates.TemplateResponse(
+        "import_wallet.html",
         {
             "request": request
         }
