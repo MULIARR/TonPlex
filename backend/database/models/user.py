751c890f638a72b331df538f6de51c6d26fa6913
MULIARR <10328055@stud.op.edu.ua>
2024-06-07 23:27:07 +0300

    Is is realy so difficult?

--- backend/database/models/user.py
+++ backend/database/models/user.py
@@ -1,9 +1,9 @@
-from sqlalchemy import Column, Integer, LargeBinary
+from sqlalchemy import Column, LargeBinary, BigInteger
 from backend.database.db import Database
 
 
 class User(Database.Base):
     __tablename__ = 'users'
 
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     mnemonic = Column(LargeBinary, nullable=False)
